7a9c0669e73911a2592b09a3cb39a1d17e7186ab
Sanduf01 <sandusanf01@gmail.com>
2026-01-17 18:56:00 +0530

    Update tailwind.config.js

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellowBrand: "#facc15",
+        darkBg: "#0b0b0b"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
